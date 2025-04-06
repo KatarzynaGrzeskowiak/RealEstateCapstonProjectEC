27751f446054432e79c18edf24ee27c07cd6741c
Katarzyna Grześkowiak <tabita33@gmail.com>
2025-04-06 18:02:47 +0200

    feat: add eslint and delete unnecessary scss and css

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'windows'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+    },
+  },
+]; 
